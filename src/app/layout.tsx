46cd2a3243a3741796e85bc8f482b2152505d634
helitonns <helitonns@gmail.com>
2024-03-22 17:35:14 -0400

    0603 - Configurando a font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { type_second } from "@/functions/fonts";
 
 export const metadata: Metadata = {
   title: "Dogs Next",
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body className={type_second.variable}>{children}</body>
     </html>
   );
 }
