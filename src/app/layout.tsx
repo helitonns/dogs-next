966f10c96dd1c1e4495dc2dba3ccece6d4864131
helitonns <helitonns@gmail.com>
2024-03-24 11:15:58 -0400

    0605 - Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { type_second } from "@/functions/fonts";
+import Header from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Dogs Next",
@@ -10,7 +11,10 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="pt-BR">
-      <body className={type_second.variable}>{children}</body>
+      <body className={type_second.variable}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
