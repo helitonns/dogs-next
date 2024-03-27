b5d9e7726e427cd4097562489a2ce6363b2ac163
helitonns <helitonns@gmail.com>
2024-03-27 17:39:13 -0400

    0610 - UseForm

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import login from "@/actions/login";
+import { useFormState, useFormStatus } from "react-dom";
+import Button from "../forms/button";
+
+function FormButton(){
+  const {pending} = useFormStatus();
+
+  
+  return (
+    <>
+      { pending ? 
+        <Button disabled={pending}>Enviando ...</Button> 
+          : 
+        <Button>Entrar</Button>}
+    </> 
+  );
+}
 
 export default function LoginForm(){
-
+  const [state, action] = useFormState(login, {
+    ok: false,
+    error: "",
+    data: null
+  });
 
   return (
     <>
-      <form action={login}>
+      <form action={action}>
         <input type="text" name="username" placeholder="Usuario"/>
         <input type="password" name="password" placeholder="Senha"/>
-        <button>Entrar</button>
-      </form>
+        <FormButton />
+        <p>{state.error}</p>
+      </form> 
     </>
   );
 }
