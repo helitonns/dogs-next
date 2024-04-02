c22fa9493603798ea4f8c6c44c85510a4cee50b3
helitonns <helitonns@gmail.com>
2024-04-02 17:50:02 -0400

    0618 - Create Context

---
+++ src/context/user-context.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { User } from "@/actions/user-get";
+import React from "react";
+
+type IUserContext = {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
+
+const UserContext = React.createContext<IUserContext | null>(null);
+
+export const useUser = ()=> {
+  const context = React.useContext(UserContext);
+  if(context === null) throw new Error("useContext deve estar dentro do Provider");
+  return context;
+}
+
+export function UserContextProvider({children, user}: {children: React.ReactNode, user: User | null}){
+  const [userState, setUser] = React.useState<User | null>(user);
+  
+  return (
+    <UserContext.Provider value={{user: userState, setUser}}>
+      {children}
+    </UserContext.Provider>
+  );
+}
