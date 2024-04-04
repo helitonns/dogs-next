8457cdc0b409f0fb46e663180d43d2e83e69abeb
helitonns <helitonns@gmail.com>
2024-04-04 17:48:02 -0400

    0621 - Photo Post

--- src/app/conta/postar/page.tsx
+++ src/app/conta/postar/page.tsx
@@ -1,7 +1,14 @@
+import ContaPhotoPost from "@/components/conta/conta-photo-post";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Postar | Minha conta",
+}
+
+export const runtime = "edge";
+
 export default async function PostarPage(){
   return (
-    <main>
-      <h1>Postar</h1>
-    </main>
+    <ContaPhotoPost />
   );
 }
