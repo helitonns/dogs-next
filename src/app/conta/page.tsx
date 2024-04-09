ff38f64e06ba10f2631edde01823aea35a9c8d90
helitonns <helitonns@gmail.com>
2024-04-09 17:14:57 -0400

    0624 - Scroll infinito

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -12,13 +12,13 @@ export default async function ContaPage(){
   const {data: user} = await userGet();
   const {data} = await photosGet({user: user?.username});
   return (
-    <main>
+    <section>
       {data?.length ? 
-        <Feed photos={data} /> : 
+        <Feed photos={data} user={user?.username} /> : 
         <div>
           <p style={{color: "#444", fontSize: "1.25rem", marginBottom: "1rem"}}>Sem fotos no momento</p>
           <Link href={"/conta/postar"} className="button" style={{display: "inline-block"}}>Postar Foto</Link>
         </div>}
-    </main>
+    </section>
   );
 }
