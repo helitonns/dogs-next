71051442af41dec17340461e248f6362a4522437
helitonns <helitonns@gmail.com>
2024-04-11 17:23:32 -0400

    0628 - Not Found

---
+++ src/app/global-error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function GlobalError(){7
+  return (
+    <html>
+      <body>
+        <h1>Um erros ocorreu.</h1>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+
+export default function NotFound(){
+  return (
+    <section className="container">
+      <h1 className="title">Página não encontrada.</h1>
+      <Link className="button" style={{margin: "1rem", display: "inline-block"}} href={"/"}>
+        Volte para a Home.
+      </Link>
+    </section>
+  );
+}
