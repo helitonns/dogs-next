0a78de8e8d67b95538df28e2bdd9335509fc600c
helitonns <helitonns@gmail.com>
2024-04-15 17:27:37 -0400

    Validate Token

--- src/context/user-context.tsx
+++ src/context/user-context.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import logout from "@/actions/logout";
 import { User } from "@/actions/user-get";
+import validateToken from "@/actions/validate-token";
 import React from "react";
 
 type IUserContext = {
@@ -19,6 +21,15 @@ export const useUser = ()=> {
 export function UserContextProvider({children, user}: {children: React.ReactNode, user: User | null}){
   const [userState, setUser] = React.useState<User | null>(user);
   
+  React.useEffect(()=> {
+    async function validate() {
+      const {ok} = await validateToken();
+      if(!ok) await logout();
+    }
+
+    if(userState) validate();
+  },[userState]);
+
   return (
     <UserContext.Provider value={{user: userState, setUser}}>
       {children}
